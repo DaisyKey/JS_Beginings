922049f2ce68739392f74d501484bb94b90b1416
Daisy <kravc-08@yandex.ru>
2025-04-18 12:02:11 +0300

    Any Variables and methods.

--- Script.js
+++ Script.js
@@ -1,6 +1,10 @@
-var txt,num
+var txt,num,x
 txt = " Через переменные"
 num = 1
+x = "Сегодня я ввел уже: "
 
 document.write("Изучение <b> JS </b> <br> Загрузка теперь из файла")//HTML теги спокойно будут обрабатываться браузером
 document.write(txt+num)
+document.write(x)
+x = 3
+document.write(x)
