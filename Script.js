275f5f6c09c426927bbad2b20687b5b6bfcae37c
Daisy <kravc-08@yandex.ru>
2025-04-18 11:55:25 +0300

    2 variables example

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
-var txt
+var txt,num
 txt = " Через переменные"
+num = 1
 
 document.write("Изучение <b> JS </b> <br> Загрузка теперь из файла")//HTML теги спокойно будут обрабатываться браузером
-document.write(txt)
+document.write(txt+num)
